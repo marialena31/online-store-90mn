018f06d481e55a0c7c6098355529b50ac40ddb95
Maria'lena Pietri <marialena.pietri@maps-system.com>
2021-12-15 21:50:23 +0100

    fix comments forbidden

--- src/components/DisplayStaticContent.js
+++ src/components/DisplayStaticContent.js
@@ -1,7 +1,7 @@
 import Strapi from "strapi-sdk-javascript";
 import React from "react";
 import {Image} from "gestalt";
-const apiUrl =/* process.env.REACT_APP_API_URL ||*/ 'http://localhost:1337'
+const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:1337'
 const strapi = new Strapi(apiUrl)
 
 function DisplayStaticContent ({name}) {
